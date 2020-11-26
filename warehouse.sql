38163a147cbfb156c048f1b1bfb0319646b7ddc2
HIKKANOTE <35695648+HIKKANOTE@users.noreply.github.com>
2020-11-26 03:15:07 +0300

    Add files via upload

---
+++ warehouse.sql
@@ -0,0 +1,31 @@
+-- TABLE
+CREATE TABLE brand (
+	id NOT NULL PRIMARY KEY,
+   	name varchar(20),
+  	country varchar(20)
+);
+CREATE TABLE goods (
+	id NOT NULL PRIMARY KEY,
+   	warehouseid varchar(20),
+  	productid varchar(20),
+  	quantity varchar(20),
+  	FOREIGN KEY (warehouseid) REFERENCES warehouse (id),
+ 	FOREIGN KEY (productid) REFERENCES product (id)
+);
+CREATE TABLE product (
+	id NOT NULL PRIMARY KEY,
+   	name varchar(20),
+  	brandid varchar(20),
+  	FOREIGN KEY (brandid) REFERENCES brand (id)
+);
+CREATE TABLE warehouse (
+	id NOT NULL PRIMARY KEY,
+   	name varchar(20)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
